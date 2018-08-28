7938342000d1365e89ee1f4cc95aa90cad650f6f
jamestouri <j_touri@yahoo.com>
2018-08-27 21:39:22 -0700

    Didn't finish hw, looking through solution

---
+++ W2D2/skeleton/lib/board.rb
@@ -0,0 +1,58 @@
+class Board
+  attr_accessor :cups
+
+  def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) {[]}
+    place_stones
+  end
+
+  def place_stones
+    @cups.each_with_index do |cup, idx|
+      unless idx == 13 || idx == 6
+        4.times do
+          cup << :stone
+        end
+      end
+    end
+  end
+
+
+
+  def valid_move?(start_pos)
+    raise "Invalid starting cup" if start_pos > 13 || start_pos < -1
+    raise "Starting cup is empty" if @cups[start_pos].empty?
+  end
+
+  def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    
+
+  end
+
+  def next_turn(ending_cup_idx)
+    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :promp
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+      ending_cup_idx
+    end
+  end
+
+  def render
+    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    puts ""
+    puts ""
+  end
+
+  def one_side_empty?
+  end
+
+  def winner
+  end
+end
